b266b4bd09bf98032a0830d4900b0fc2773f3590
Yura <tushe.s.y@gmail.com>
2020-11-22 18:48:23 +0200

    added tabs and with footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
-let value=document.querySelectorAll('.catalog-item__content');
+
+$(document).ready(function(){
+    let value=document.querySelectorAll('.catalog-item__content');
 for (let item of value){
     item.classList.add('catalog-item__content_active');
   }
-$(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -25,20 +26,7 @@ $(document).ready(function(){
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-    // $('.catalog-item__link').each(function(i){
-    //     $(this).on('click',(e)=>{
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     });
-    // });
-    // $('.catalog-item__back').each(function(i){
-    //     $(this).on('click',(e)=>{
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     });
-    // });
+ 
     function toogleSlide(item){
         $(item).each(function(i){
             $(this).on('click',(e)=>{
@@ -50,6 +38,25 @@ $(document).ready(function(){
     };
     toogleSlide('.catalog-item__link');
     toogleSlide('.catalog-item__back');
+
+    ////modal
+    $('[data-modal=consultation]').on('click', ()=>{
+        $('.overlay, #consultation'  ).fadeIn('fast');
+    });
+
+
+
+    $('.modal__close').on('click', ()=>{
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+  
+    $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+
     
   });
   
