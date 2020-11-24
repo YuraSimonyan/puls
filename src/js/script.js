6fd40f40985cea4bada9042355b91a9766238166
Yura <tushe.s.y@gmail.com>
2020-11-24 15:58:20 +0200

    fixed valid, added animations

--- src/js/script.js
+++ src/js/script.js
@@ -103,16 +103,16 @@ for (let item of value){
        }else{
            $('.pageup').fadeOut();
        }
-       $("a[href^='#']").click(function(){
-        let _href = $(this).attr("href");
+       
+    });
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
-});
-    });
-    
+        });
  
        
- 
+    new WOW().init();
  
   });
   
